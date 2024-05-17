2c26d23b0ca6b7688e498ccb361878b13f40dd8b
Jonathon Tait <jttait@gmail.com>
2024-05-17 07:58:41 +0100

    Leetcode problem 2812

---
+++ src/main/java/leetcode/problem2812/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.problem2812;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+        List<List<Integer>> safenessGrid = generateSafenessGrid(grid);
+        return 0;
+    }
+
+    private List<List<Integer>> generateSafenessGrid(List<List<Integer>> grid) {
+        
+
+        List<List<Integer>> result = new ArrayList<>();
+        return result;
+    }
+}
---
+++ src/test/java/leetcode/problem2812/SolutionTest.java
@@ -0,0 +1,34 @@
+package leetcode.problem2812;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+
+class SolutionTest {
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void shouldBe0for1by1gridWithThief() {
+        List<List<Integer>> grid = List.of(List.of(1));
+        int result = solution.maximumSafenessFactor(grid);
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    void shouldBe1for2by2gridWithThief() {
+        List<List<Integer>> grid = List.of(
+                List.of(0, 1),
+                List.of(0, 0)
+        );
+        int result = solution.maximumSafenessFactor(grid);
+        Assertions.assertEquals(1, result);
+    }
+}
