307f4c6b8045bd1195e6c56158e23df1b5be656e
Jonathon Tait <jttait@gmail.com>
2024-12-15 08:38:18 +0000

    Advent of Code 2024 Problem 4

---
+++ src/test/java/adventofcode/year2024/problem4/TestUtils.java
@@ -0,0 +1,24 @@
+package adventofcode.year2024.problem4;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestUtils {
+    public List<List<Character>> loadFile(String filename) throws Exception {
+        List<List<Character>> result = new ArrayList<>();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filename);
+        InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        BufferedReader reader = new BufferedReader(streamReader);
+        for (String line; (line = reader.readLine()) != null;) {
+            result.add(new ArrayList<>());
+            for (char c : line.toCharArray()) {
+                result.get(result.size() - 1).add(c);
+            }
+        }
+        return result;
+    }
+}
