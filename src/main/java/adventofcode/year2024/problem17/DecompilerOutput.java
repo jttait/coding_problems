9fd9a3f8a1ec8c5dbdc66b724522f2692c537fcd
Jonathon Tait <jttait@gmail.com>
2025-03-06 20:52:29 +0000

    Delete unused classes

--- src/main/java/adventofcode/year2024/problem17/DecompilerOutput.java
+++
@@ -1,3 +0,0 @@
-package adventofcode.year2024.problem17;
-
-public record DecompilerOutput(int a, int out) {}
--- src/main/java/adventofcode/year2024/problem17/State.java
+++
@@ -1,3 +0,0 @@
-package adventofcode.year2024.problem17;
-
-public record State(int instructionPointer, int expectedOutput) {}
