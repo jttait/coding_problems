e5c039059bb1bc9096192a8fcbf2ed3ca7647ec9
Jonathon Tait <jttait@gmail.com>
2025-03-16 06:48:09 +0000

    Tidy up problem 21

--- src/main/java/adventofcode/year2024/problem21/PartOne.java
+++ src/main/java/adventofcode/year2024/problem21/PartOne.java
@@ -1,52 +1,32 @@
 package adventofcode.year2024.problem21;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class PartOne {
-    public int shortestPath(List<Character> buttons) {
-        Keypad numericKeypad = new Keypad(Grid.NUMERIC);
-        Keypad directionalKeypad = new Keypad(Grid.DIRECTIONAL);
+    private final List<Keypad> keypads;
 
-        List<List<Character>> shortestPathsForDepressurisedKeypad = cartesianProduct(numericKeypad, buttons);
-
-        List<List<Character>> hello = new ArrayList<>();
-        for (List<Character> list : shortestPathsForDepressurisedKeypad) {
-            hello.addAll(cartesianProduct(directionalKeypad, list));
+    public PartOne(int numRobotControlledKeypads) {
+        this.keypads = new ArrayList<>();
+        keypads.add(new Keypad(Grid.NUMERIC));
+        for (int i = 0; i < numRobotControlledKeypads; i++) {
+            keypads.add(new Keypad(Grid.DIRECTIONAL));
         }
-
-        int minLength = hello.stream().map(List::size).min(Comparator.comparingInt(a -> a)).get();
-        hello = hello.stream().filter(a -> a.size() == minLength).collect(Collectors.toList());
-
-        int result = Integer.MAX_VALUE;
-        for (List<Character> list : hello) {
-            List<List<Character>> shortest = cartesianProduct(directionalKeypad, list);
-            int minLength2 = shortest.stream().map(List::size).min(Comparator.comparingInt(a -> a)).get();
-            result = Math.min(result, minLength2);
+        for (int i = 0; i < keypads.size() - 1; i++) {
+            keypads.get(i).setPrecedingKeypad(keypads.get(i + 1));
         }
-
-        return result;
     }
 
-    private List<List<Character>> cartesianProduct(Keypad keypad, List<Character> buttons) {
-        List<List<Character>> shortestPaths = keypad.shortestPaths('A', buttons.get(0));
-        char current = buttons.get(0);
-        for (int i = 1; i < buttons.size(); i++) {
-            List<List<Character>> paths = keypad.shortestPaths(current, buttons.get(i));
-            List<List<Character>> shortestPaths2 = new ArrayList<>();
-            for (List<Character> shortestPath : shortestPaths) {
-                for (List<Character> path : paths) {
-                    List<Character> shortestPath2 = new ArrayList<>(shortestPath);
-                    shortestPath2.addAll(path);
-                    shortestPaths2.add(List.copyOf(shortestPath2));
-                    shortestPath2.subList(shortestPath2.size() - path.size(), shortestPath2.size());
-                }
-            }
-            shortestPaths = shortestPaths2;
-            current = buttons.get(i);
+    public long solve(String s) {
+        char[] array = s.toCharArray();
+        List<Character> buttons = new ArrayList<>();
+        buttons.add('A');
+        for (char c : array) {
+            buttons.add(c);
         }
-        return shortestPaths;
+        long result = 0;
+        for (int i = 0; i < buttons.size() - 1; i++) {
+            result += keypads.get(0).solve(buttons.get(i), buttons.get(i + 1));
+        }
+        return result;
     }
 }
--- src/test/java/adventofcode/year2024/problem21/PartOneTest.java
+++ src/test/java/adventofcode/year2024/problem21/PartOneTest.java
@@ -3,98 +3,100 @@ package adventofcode.year2024.problem21;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 class PartOneTest {
     @Test
-    void shouldBe68for029A() {
-        List<Character> buttons = List.of('0', '2', '9', 'A');
-        PartOne partOne = new PartOne();
-        int shortestPath = partOne.shortestPath(buttons);
-        Assertions.assertEquals(68, shortestPath);
+    void shouldBe12for029AwithNoRobots() {
+        PartOne partOne = new PartOne(0);
+        long result = partOne.solve("029A");
+        Assertions.assertEquals(12, result);
     }
 
     @Test
-    void shouldBe60for980A() {
-        List<Character> buttons = List.of('9', '8', '0', 'A');
-        PartOne partOne = new PartOne();
-        int shortestPath = partOne.shortestPath(buttons);
-        Assertions.assertEquals(60, shortestPath);
+    void shouldBe28for029AwithOneRobots() {
+        PartOne partOne = new PartOne(1);
+        long result = partOne.solve("029A");
+        Assertions.assertEquals(28, result);
     }
 
     @Test
-    void shouldBe68for179A() {
-        List<Character> buttons = List.of('1', '7', '9', 'A');
-        PartOne partOne = new PartOne();
-        int shortestPath = partOne.shortestPath(buttons);
-        Assertions.assertEquals(68, shortestPath);
+    void shouldBe68for029AwithTwoRobots() {
+        PartOne partOne = new PartOne(2);
+        long result = partOne.solve("029A");
+        Assertions.assertEquals(68, result);
     }
 
     @Test
-    void shouldBe64for456A() {
-        List<Character> buttons = List.of('4', '5', '6', 'A');
-        PartOne partOne = new PartOne();
-        int shortestPath = partOne.shortestPath(buttons);
-        Assertions.assertEquals(64, shortestPath);
+    void shouldBe60for980AwithTwoRobots() {
+        PartOne partOne = new PartOne(2);
+        long result = partOne.solve("980A");
+        Assertions.assertEquals(60, result);
     }
 
     @Test
-    void shouldBe64for379A() {
-        List<Character> buttons = List.of('3', '7', '9', 'A');
-        PartOne partOne = new PartOne();
-        int shortestPath = partOne.shortestPath(buttons);
-        Assertions.assertEquals(64, shortestPath);
+    void shouldBe68for179AwithTwoRobots() {
+        PartOne partOne = new PartOne(2);
+        long result = partOne.solve("179A");
+        Assertions.assertEquals(68, result);
     }
 
     @Test
-    void shouldBe70for539A() {
-        List<Character> buttons = List.of('5', '3', '9', 'A');
-        PartOne partOne = new PartOne();
-        int shortestPath = partOne.shortestPath(buttons);
-        Assertions.assertEquals(70, shortestPath);
+    void shouldBe64for456AwithTwoRobots() {
+        PartOne partOne = new PartOne(2);
+        long result = partOne.solve("456A");
+        Assertions.assertEquals(64, result);
     }
 
     @Test
-    void shouldBe72for964A() {
-        List<Character> buttons = List.of('9', '6', '4', 'A');
-        PartOne partOne = new PartOne();
-        int shortestPath = partOne.shortestPath(buttons);
-        Assertions.assertEquals(72, shortestPath);
+    void shouldBe64for379AwithTwoRobots() {
+        PartOne partOne = new PartOne(2);
+        long result = partOne.solve("379A");
+        Assertions.assertEquals(64, result);
     }
 
     @Test
-    void shouldBe76for803A() {
-        List<Character> buttons = List.of('8', '0', '3', 'A');
-        PartOne partOne = new PartOne();
-        int shortestPath = partOne.shortestPath(buttons);
-        Assertions.assertEquals(76, shortestPath);
+    void shouldBe70for539AwithTwoRobots() {
+        PartOne partOne = new PartOne(2);
+        long result = partOne.solve("539A");
+        Assertions.assertEquals(70, result);
     }
 
     @Test
-    void shouldBe76for149A() {
-        List<Character> buttons = List.of('1', '4', '9', 'A');
-        PartOne partOne = new PartOne();
-        int shortestPath = partOne.shortestPath(buttons);
-        Assertions.assertEquals(76, shortestPath);
+    void shouldBe72for964AwithTwoRobots() {
+        PartOne partOne = new PartOne(2);
+        long result = partOne.solve("964A");
+        Assertions.assertEquals(72, result);
     }
 
     @Test
-    void shouldBe66for789A() {
-        List<Character> buttons = List.of('7', '8', '9', 'A');
-        PartOne partOne = new PartOne();
-        int shortestPath = partOne.shortestPath(buttons);
-        Assertions.assertEquals(66, shortestPath);
+    void shouldBe76for803AwithTwoRobots() {
+        PartOne partOne = new PartOne(2);
+        long result = partOne.solve("803A");
+        Assertions.assertEquals(76, result);
+    }
+
+    @Test
+    void shouldBe76for149AwithTwoRobots() {
+        PartOne partOne = new PartOne(2);
+        long result = partOne.solve("149A");
+        Assertions.assertEquals(76, result);
+    }
+
+    @Test
+    void shouldBe66for789AwithTwoRobots() {
+        PartOne partOne = new PartOne(2);
+        long result = partOne.solve("803A");
+        Assertions.assertEquals(76, result);
     }
 
     @Test
     void shouldBe231564forAdventOfCodeInput() {
-        PartOne partOne = new PartOne();
-        int result = 0;
-        result += partOne.shortestPath(List.of('5', '3', '9', 'A')) * 539;
-        result += partOne.shortestPath(List.of('9', '6', '4', 'A')) * 964;
-        result += partOne.shortestPath(List.of('8', '0', '3', 'A')) * 803;
-        result += partOne.shortestPath(List.of('1', '4', '9', 'A')) * 149;
-        result += partOne.shortestPath(List.of('7', '8', '9', 'A')) * 789;
+        PartOne partOne = new PartOne(2);
+        long result = 0;
+        result += partOne.solve("539A") * 539;
+        result += partOne.solve("964A") * 964;
+        result += partOne.solve("803A") * 803;
+        result += partOne.solve("149A") * 149;
+        result += partOne.solve("789A") * 789;
         Assertions.assertEquals(231564, result);
     }
 }
