67429fad6ccf813c19c183f4aea2c236f84bdabf
Jonathon Tait <jttait@gmail.com>
2025-12-04 20:09:25 +0000

    Advent of Code 2025 Day 1 (#60)
    * Passed day 1
    
    * Upgrade JUnit
    
    * Add JUnit platform launcher

---
+++ src/main/java/adventofcode/year2025/problem01/PartOne.java
@@ -0,0 +1,33 @@
+package adventofcode.year2025.problem01;
+
+import java.util.List;
+
+public class PartOne {
+    public int updateDial(int current, String command) {
+        char direction = command.charAt(0);
+        int magnitude = Integer.parseInt(command.substring(1));
+        if (direction == 'L') {
+            current -= magnitude;
+        } else {
+            current += magnitude;
+        }
+        if (current >= 100) {
+            current %= 100;
+        } else if (current < 0) {
+            current = (Math.abs(current % 100) == 0) ? 0 : 100 - Math.abs(current % 100);
+        }
+        return current;
+    }
+
+    public int countZeroes(List<String> commands) {
+        int current = 50;
+        int result = 0;
+        for (String command : commands) {
+            current = updateDial(current, command);
+            if (current == 0) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/adventofcode/year2025/problem01/PartTwo.java
@@ -0,0 +1,44 @@
+package adventofcode.year2025.problem01;
+
+import java.util.List;
+
+public class PartTwo {
+    public int updateDial(int current, String command) {
+        char direction = command.charAt(0);
+        int magnitude = Integer.parseInt(command.substring(1));
+        if (direction == 'L') {
+            current -= magnitude;
+        } else {
+            current += magnitude;
+        }
+        return current;
+    }
+
+    public int countAllZeroes(List<String> commands) {
+        int current = 50;
+        int result = 0;
+        for (String command : commands) {
+            int updated = updateDial(current, command);
+            if (current > 0 && updated < 0) {
+                result++;
+                result += (current / 100) - (updated / 100);
+            } else if (current < 0 && updated > 0) {
+                result++;
+                result += (updated / 100) - (current / 100);
+            } else {
+                result += Math.abs((updated/100) + (current/100));
+            }
+            if (updated == 0) {
+                result++;
+            }
+
+            if (updated >= 100) {
+                updated %= 100;
+            } else if (updated < 0) {
+                updated = (Math.abs(updated % 100) == 0) ? 0 : 100 - Math.abs(updated % 100);
+            }
+            current = updated;
+        }
+        return result;
+    }
+}
