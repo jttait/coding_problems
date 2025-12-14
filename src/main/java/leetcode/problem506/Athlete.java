88ead6df910c9ae198d3650590013a7b899744ce
Jonathon Tait <jttait@gmail.com>
2025-12-14 20:41:59 +0000

    Delete Advent of Code 2024 from root src

--- src/main/java/leetcode/problem506/Athlete.java
+++
@@ -1,7 +0,0 @@
-package leetcode.problem506;
-
-public record Athlete(int index, int score) implements Comparable<Athlete> {
-    public int compareTo(Athlete o) {
-        return o.score() - this.score();
-    }
-}
--- src/main/java/leetcode/problem58/Solution.java
+++
@@ -1,8 +0,0 @@
-package leetcode.problem58;
-
-public class Solution {
-    public int lengthOfLastWord(String s) {
-        int indexOfLastSpace = s.lastIndexOf(' ');
-        return s.length() - indexOfLastSpace - 1;
-    }
-}
