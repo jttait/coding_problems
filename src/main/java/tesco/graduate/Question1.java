7e2d1c77c76a33dee1deb476e2e1c22ea008506f
Jonathon Tait <jttait@gmail.com>
2025-12-14 21:05:44 +0000

    Delete src

--- src/main/java/tesco/graduate/Question1.java
+++
@@ -1,9 +0,0 @@
-package tesco.graduate;
-
-import java.util.List;
-
-public class Question1 {
-    public static int sum(List<Integer> list) {
-        return list.stream().reduce(0, Integer::sum);
-    }
-}
