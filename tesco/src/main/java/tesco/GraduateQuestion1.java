db67ac1f4dd31508fbf3a0a37879c5c15f1607d5
Jonathon Tait <jttait@gmail.com>
2026-01-10 17:22:40 +0000

    Remove Tesco

--- tesco/src/main/java/tesco/GraduateQuestion1.java
+++
@@ -1,9 +0,0 @@
-package tesco;
-
-import java.util.List;
-
-public class GraduateQuestion1 {
-    public static int sum(List<Integer> list) {
-        return list.stream().reduce(0, Integer::sum);
-    }
-}
